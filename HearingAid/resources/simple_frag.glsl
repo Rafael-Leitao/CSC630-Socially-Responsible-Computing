7522f3706580012e418105afb7269ef9f9c9e69e
MayaBal <111805337+MayaBal@users.noreply.github.com>
2024-04-17 16:33:08 -0700

    frag shader added - Maya

--- HearingAid/resources/simple_frag.glsl
+++ HearingAid/resources/simple_frag.glsl
@@ -1,16 +1,14 @@
-#version 330 core 
+#version 330 core
 in vec3 fragNor;
 out vec4 color;
 
 uniform vec3 MatAmb;
+uniform vec3 lightP;
 
 void main()
 {
 	vec3 normal = normalize(fragNor);
-	// fix these
-	//float dc = ?;
-	//vec3 halfV = ?;
-	//float sc = ?;
-	color = vec4(MatAmb, 1.0);
-
+	vec3 normalLight = normalize(lightP);
+	float cosTheta = dot(normal, normalLight);
+	color = vec4(cosTheta * MatAmb, 1.0);
 }
